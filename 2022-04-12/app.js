3d5842c35b5df188cf638a82282663b639951a67
mrsmarr <bekkemarr@gmail.com>
2022-04-14 09:52:23 -0600

    Square(n) Sum

---
+++ 2022-04-12/app.js
@@ -0,0 +1,9 @@
+//8kyu `Square(n) Sum`
+//Note: Coded along with `HelloItsRufio`
+
+//Complete the square sum function so that it squares each number passed into it and then sums the results together.
+//For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+
+function squareSum(arr){
+    return arr.map(num => num **2).reduce((acc,c)=>acc+c,0)
+  }
