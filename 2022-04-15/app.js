43272d92d4917b97e7849a43b99ff39999e95e0b
mrsmarr <bekkemarr@gmail.com>
2022-04-15 18:00:07 -0600

    Convert a Number to a String!

---
+++ 2022-04-15/app.js
@@ -0,0 +1,11 @@
+//8kyu `Convert a Number to a String!`
+
+//We need a function that can transform a number into a string.
+
+//What ways of achieving this do you know?
+
+function numberToString(num) {
+    // Return a string of the number here!
+    let stringNum = num.toString()
+    return stringNum
+  }
