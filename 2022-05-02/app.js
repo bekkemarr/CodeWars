2fd16af49665cbd45bf14525db20211e3f544557
mrsmarr <bekkemarr@gmail.com>
2022-05-02 16:55:55 -0600

    2022-05-02

---
+++ 2022-05-02/app.js
@@ -0,0 +1,10 @@
+//8kyu `Reversed Strings`
+
+function solution(str){
+    //P String
+    //R String but reversed
+    //E world -> dlrow word->drow honda ->adnoh
+    //P literally just reverse the string
+      let newStr = str.split('').reverse().join('')
+    return newStr
+    }
