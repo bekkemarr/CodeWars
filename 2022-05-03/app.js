2694e485ce70268269443701a9c2bf72ff12c066
mrsmarr <bekkemarr@gmail.com>
2022-05-03 16:58:23 -0600

    2022-05-03

---
+++ 2022-05-03/app.js
@@ -0,0 +1,13 @@
+//8kyu `Remove First and Last Character`
+
+function removeChar(str){
+    //You got this!
+   //P STRING
+   //R String but ends are chopped off
+   //E PREP -> RE  HONDA -> OND  MRSMARR -> RSMAR
+   //P .removeChar(1, .length-1?)
+   // Create an array -> some of the methods create a new array
+     let newStr = str.split('').slice(1, str.length-1).join('')
+     return newStr
+   };
+   
