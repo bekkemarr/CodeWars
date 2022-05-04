26e995c64851b05d34aaa51ec86b1c90fe4cbb0e
mrsmarr <bekkemarr@gmail.com>
2022-05-04 16:58:36 -0600

    2022-05-04

---
+++ 2022-05-04/app.js
@@ -0,0 +1,10 @@
+//8kyu `Opposite Number`
+
+function opposite(number) {
+    //your code here
+    //P Number either positive or negative
+    //R Said number reverse the sign (+/-)
+    //E 6 -> -6, -14 -> 14, 2 -> -2
+    //P return opposite number 
+    return number * -1
+  }
