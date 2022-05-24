ea1de4ee1b0e3a56b9ebb83ebdab09feab9aa9f2
mrsmarr <bekkemarr@gmail.com>
2022-05-24 17:57:40 -0600

    Training JS #1: create your first JS function

---
+++ 2022-05-24/app.js
@@ -0,0 +1,13 @@
+//8kyu `Training JS #1: create your first JS function and print "Hello World!"`
+
+//refer to the example and write your first JS function
+
+//P can be blank
+//R "hello world"
+//E console.log("Hello world!")
+//P Write a function that results in hello world
+
+function helloWorld(){
+    let str='Hello World!'
+    console.log(str)
+  }
