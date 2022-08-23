f5d813606aa687dbc9c0a9179184bd435e933f00
mrsmarr <bekkemarr@gmail.com>
2022-08-23 13:13:11 -0600

    Get the mean of an array

---
+++ 2022-08-23/app.js
@@ -0,0 +1,16 @@
+//8kyu `Get the mean of an array`
+
+//It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+//Return the average of the given array rounded down to its nearest integer.
+//The array will never be empty.
+
+//P 
+//R 
+//E 
+//P 
+
+// function getAverage(arr){
+//     return Math.floor(arr.reduce((a,c)=>a+c )/ arr.length)
+// }
+
+const getAverage = arr => Math.floor(arr.reduce((a,c)=>a+c )/ arr.length)
