d418d35d2055ac934cad387027f9ae983d69902b
mrsmarr <bekkemarr@gmail.com>
2022-09-09 10:16:37 -0600

    MakeUpperCase

---
+++ 2022-09-09/app.js
@@ -0,0 +1,13 @@
+//8kyu `MakeUpperCase`
+
+//Write a function which converts the input string to uppercase.
+
+//P String
+//R UPPERCASE String
+//E "hello" -> "HELLO"
+//P .toUppercase
+
+function makeUpperCase(str) {
+    return str.toUpperCase()
+  }
+
