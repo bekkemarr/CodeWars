559a454b72454b1cb18bda2c6a02e618ecb28a05
mrsmarr <bekkemarr@gmail.com>
2022-09-14 17:01:21 -0600

    Convert a Boolean to a String

---
+++ 2022-08-14/app.js
@@ -0,0 +1,18 @@
+//8kyu `Convert a Boolean to a String`
+
+//Implement a function which convert the given boolean value into its string representation.
+
+//Note: Only valid inputs will be given.
+
+//P boolean
+//R string
+//E boolean true -> "true"; boolean false -> "false"
+//P there is probably a method for this
+
+let booleanToString = (b) => {
+    if(b == true){
+      return "true"
+    }else if ( b == false){
+      return "false"
+    }
+  }
